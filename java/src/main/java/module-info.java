496167f94715f502504f5637c5f5d62d7d8cf417
Johan Vos <johan@lodgon.com>
2022-09-16 14:24:17 +0200

    add missing story file + expose package

--- java/src/main/java/module-info.java
+++ java/src/main/java/module-info.java
@@ -34,6 +34,7 @@ module org.whispersystems.service {
     exports org.whispersystems.util;
     opens org.whispersystems.signalservice.api.groupsv2 to com.fasterxml.jackson.databind;
     opens org.whispersystems.signalservice.internal.push to com.fasterxml.jackson.databind;
+    opens org.whispersystems.signalservice.api.messages.calls to com.fasterxml.jackson.databind;
     opens org.whispersystems.signalservice.api.profiles to com.fasterxml.jackson.databind;
     opens org.whispersystems.signalservice.api.push to com.fasterxml.jackson.databind;
     opens org.whispersystems.signalservice.api.storage to com.fasterxml.jackson.databind;
