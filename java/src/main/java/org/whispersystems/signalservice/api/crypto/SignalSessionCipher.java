d4b4a307e67ef9a958026c800356c72fa0c39d0b
Johan Vos <johan@lodgon.com>
2022-11-20 11:26:10 +0100

    Fixprofiletest (#5)
    * fix ProfileCipherTest
    
    * Build version 42
    
    * prepare next snapshot

--- java/src/main/java/org/whispersystems/signalservice/api/crypto/SignalSessionCipher.java
+++ java/src/main/java/org/whispersystems/signalservice/api/crypto/SignalSessionCipher.java
@@ -1,7 +1,5 @@
 package org.whispersystems.signalservice.api.crypto;
 
-//import org.whispersystems.libsignal.DuplicateMessageException;
-
 import org.signal.libsignal.protocol.DuplicateMessageException;
 import org.signal.libsignal.protocol.InvalidKeyException;
 import org.signal.libsignal.protocol.InvalidKeyIdException;
@@ -10,23 +8,11 @@ import org.signal.libsignal.protocol.InvalidVersionException;
 import org.signal.libsignal.protocol.LegacyMessageException;
 import org.signal.libsignal.protocol.NoSessionException;
 import org.signal.libsignal.protocol.SessionCipher;
+import org.signal.libsignal.protocol.UntrustedIdentityException;
 import org.signal.libsignal.protocol.message.CiphertextMessage;
 import org.signal.libsignal.protocol.message.PreKeySignalMessage;
-import org.whispersystems.signalservice.api.SignalSessionLock;
-import org.signal.libsignal.protocol.UntrustedIdentityException;
 import org.signal.libsignal.protocol.message.SignalMessage;
-
-//import org.whispersystems.libsignal.InvalidKeyException;
-//import org.whispersystems.libsignal.InvalidKeyIdException;
-//import org.whispersystems.libsignal.InvalidMessageException;
-//import org.whispersystems.libsignal.LegacyMessageException;
-//import org.whispersystems.libsignal.NoSessionException;
-//import org.whispersystems.libsignal.SessionCipher;
-//import org.whispersystems.libsignal.UntrustedIdentityException;
-//import org.whispersystems.libsignal.protocol.CiphertextMessage;
-//import org.whispersystems.libsignal.protocol.PreKeySignalMessage;
-//import org.whispersystems.libsignal.protocol.SignalMessage;
-//import org.whispersystems.signalservice.api.SignalSessionLock;
+import org.whispersystems.signalservice.api.SignalSessionLock;
 
 /**
  * A thread-safe wrapper around {@link SessionCipher}.
